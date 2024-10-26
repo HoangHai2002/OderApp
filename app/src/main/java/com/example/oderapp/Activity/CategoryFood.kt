6c88a3a3fdb6d0ff5ecec79d99d246164e61dfdc
hieu <tdh203.hieu@gmail.com>
2024-10-26 18:16:44 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/oderapp/Fragment/Tatca_Fragment.kt

--- app/src/main/java/com/example/oderapp/Activity/CategoryFood.kt
+++ app/src/main/java/com/example/oderapp/Activity/CategoryFood.kt
@@ -42,7 +42,51 @@ class CategoryFood : AppCompatActivity() {
         bind.rcCategoryFood.layoutManager = LinearLayoutManager(this)
         bind.rcCategoryFood.adapter = adapter
 
+        bind.btnAddCategory.setOnClickListener{
+            val dialog = layoutInflater.inflate(R.layout.dialog_add_category, null)
+            val alertDialog = AlertDialog.Builder(this).setView(dialog).create()
 
+            val edtNameCategory = dialog.findViewById<EditText>(R.id.edtCategoryName)
+            val addCategory = dialog.findViewById<Button>(R.id.addCategory)
+            addCategory.setOnClickListener{
+                val data = dataBase.push()
+                val categoryId = data.key
+                var loaiMonAn = LoaiMonAn(
+                    categoryId,
+                    edtNameCategory.text.toString()
+                )
+                dataBase.addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (snapshot.exists()){
+                            for (category in snapshot.children){
+                                val checkCategoryName = category.child("name").getValue(String::class.java)
+                                if (edtNameCategory.text.toString().trim().isEmpty()){
+                                    Toast.makeText(applicationContext, "vui lòng nhập tên loại món", Toast.LENGTH_LONG).show()
+                                    return
+                                }
+                                if (edtNameCategory.text.toString().trim().equals(checkCategoryName)){
+                                    Toast.makeText(applicationContext, "Loại món ăn đã tồn tại", Toast.LENGTH_LONG).show()
+                                    return
+                                }
+                            }
+                            data.setValue(loaiMonAn).addOnSuccessListener {
+                                Toast.makeText(applicationContext, "Thêm loại món ăn thành công", Toast.LENGTH_SHORT).show()
+                                listCategory.clear()
+                                adapter.getCategoryData()
+                                alertDialog.dismiss()
+                            }.addOnFailureListener {
+                                Toast.makeText(applicationContext, "Thêm Thất bại", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            }
+            alertDialog.show()
+        }
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
